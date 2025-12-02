d1867929d66c732c4a544fe9a7ac71bfbdb3ecb0
KKK-member <endeladzedato123@gmail.com>
2025-12-02 12:05:23 +0400

    Add files via upload

---
+++ დავით ენდელაძე/script.js
@@ -0,0 +1,17 @@
+let i =prompt('შეიყვანეთ ტქვენი ქულა');
+if (i >=90){
+    alert('A');
+}
+else if (i > 100){
+    alert('ხელთავიდან შეიყვანეთ');
+}
+else if (i  >= 70 ) {
+    alert('B');
+}
+else if ( i >= 50){
+    alert('C');
+
+}
+else if (i <= 49){
+    alert('F');
+}
